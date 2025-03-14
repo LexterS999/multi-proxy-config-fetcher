e7c81b8d246c4b31e72290f74f882c2289084cf2
LexterS999 <kaliostro18@gmail.com>
2025-03-14 19:46:21 +0500

    Update user_settings.py

--- src/user_settings.py
+++ src/user_settings.py
@@ -3,15 +3,12 @@
 # List of source URLs to fetch proxy configurations from.
 # Add or remove URLs as needed. All URLs in this list are automatically enabled.
 SOURCE_URLS = [
-    "https://raw.githubusercontent.com/4n0nymou3/ss-config-updater/refs/heads/main/configs.txt",
-    "https://raw.githubusercontent.com/mahsanet/MahsaFreeConfig/refs/heads/main/mtn/sub_1.txt",
-    # "https://raw.githubusercontent.com/yebekhe/vpn-fail/main/sub-link",
     "https://t.me/s/FreeV2rays",
     "https://t.me/s/v2ray_free_conf",
     "https://t.me/s/PrivateVPNs",
     "https://t.me/s/IP_CF_Config",
     "https://t.me/s/shadowproxy66",
-    # "https://t.me/s/OutlineReleasedKey",
+    "https://t.me/s/OutlineReleasedKey",
     "https://t.me/s/prrofile_purple",
     "https://t.me/s/proxy_shadosocks",
     "https://t.me/s/meli_proxyy",
@@ -21,13 +18,61 @@ SOURCE_URLS = [
     "https://t.me/s/heyatserver",
     "https://t.me/s/vpnfail_vless",
     "https://t.me/s/DailyV2RY",
-    # "https://t.me/s/ShadowsocksM",
-    # Add more URLs here if you want to include additional sources.
+    "https://t.me/s/vip_vpn_2022",
+    "https://t.me/s/prrofile_purple",
+    "https://t.me/s/VlessConfig",
+    "https://t.me/s/heyatserver",
+    "https://t.me/s/v2ray_free_conf",
+    "https://t.me/s/ServerNett",
+    "https://t.me/s/ultrasurf_12",
+    "https://t.me/s/DirectVPN",
+    "https://t.me/s/shadowproxy66",
+    "https://t.me/s/proxystore11",
+    "https://t.me/s/custom_14",
+    "https://t.me/s/vpnmasi",
+    "https://t.me/s/IP_CF_Config",
+    "https://t.me/s/proxy_shadosocks",
+    "https://t.me/s/ParsRoute",
+    "https://t.me/s/DailyV2RY",
+    "https://t.me/s/Hope_Net",
+    "https://t.me/s/meli_proxyy",
+    "https://t.me/s/PrivateVPNs",
+    "https://t.me/s/vpn_ocean",
+    "https://t.me/s/FreeV2rays",
+    "https://t.me/s/TUICity",
+    "https://t.me/s/FreeVlessVpn",
+    "https://t.me/s/vmessiran",
+    "https://t.me/s/V2pedia",
+    "https://t.me/s/FreakConfig",
+    "https://t.me/s/Configforvpn01",
+    "https://t.me/s/v2rayan",
+    "https://t.me/s/ViProxys",
+    "https://t.me/s/V2Ray_FreedomIran",
+    "https://t.me/s/ShadowSocks_s",
+    "https://t.me/s/Easy_Free_VPN",
+    "https://t.me/s/ShadowsocksM",
+    "https://t.me/s/free4allVPN",
+    "https://t.me/s/VmessProtocol",
+    "https://t.me/s/Awlix_ir",
+    "https://t.me/s/iranvpnet",
+    "https://t.me/s/vpn_proxy_custom",
+    "https://t.me/s/shadowsocksshop",
+    "https://t.me/s/VPNCUSTOMIZE",
+    "https://t.me/s/vmess_vless_v2rayng",
+    "https://t.me/s/V2rayNG3",
+    "https://t.me/s/MsV2ray",
+    "https://t.me/s/vpnfail_vless",
+    "https://t.me/s/frev2rayng",
+    "https://t.me/s/vmess_iran",
+    "https://t.me/s/configV2rayForFree",
+    "https://t.me/s/DigiV2ray",
+    "https://t.me/s/ViPVpn_v2ray",
+    "https://t.me/s/yaney_01"    
 ]
 
 # Set to True to fetch the maximum possible number of configurations.
 # If True, SPECIFIC_CONFIG_COUNT will be ignored.
-USE_MAXIMUM_POWER = False
+USE_MAXIMUM_POWER = True
 
 # Desired number of configurations to fetch.
 # This is used only if USE_MAXIMUM_POWER is False.
@@ -39,12 +84,12 @@ ENABLED_PROTOCOLS = {
     "wireguard://": False,
     "hysteria2://": True,
     "vless://": True,
-    "vmess://": True,
+    "vmess://": False,
     "ss://": True,
-    "trojan://": True,
+    "trojan://": False,
     "tuic://": True,
 }
 
 # Maximum age of configurations in days.
 # Configurations older than this will be considered invalid.
-MAX_CONFIG_AGE_DAYS = 7
+MAX_CONFIG_AGE_DAYS = 30
