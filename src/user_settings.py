e97a72b224c1a1a3a3a4d4fb53a866ec79a7a83d
LexterS999 <kaliostro18@gmail.com>
2025-06-15 14:26:18 +0500

    Update user_settings.py

--- src/user_settings.py
+++ src/user_settings.py
@@ -4,6 +4,21 @@
 # Add or remove URLs as needed. All URLs in this list are automatically enabled.
 SOURCE_URLS = [
     "https://t.me/s/DeamNet_proxy",
+    "https://t.me/s/v2rayfree",
+    "https://t.me/s/FreeV2rays",
+    "https://t.me/s/v2ray_free_conf",
+    "https://t.me/s/PrivateVPNs",
+    "https://t.me/s/IP_CF_Config",
+    "https://t.me/s/prrofile_purple",
+    "https://t.me/s/meli_proxyy",
+    "https://t.me/s/DirectVPN",
+    "https://t.me/s/VmessProtocol",
+    "https://t.me/s/vpnfail_vless",
+    "https://t.me/s/DailyV2RY",
+    "https://t.me/s/shadowproxy66",
+    "https://t.me/s/moftconfig",
+    "https://t.me/s/ConfigsHUB2",
+    "https://t.me/s/ArV2ray",  
 ]
 
 # Set to True to fetch the maximum possible number of configurations.
